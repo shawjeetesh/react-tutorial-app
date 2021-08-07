d4f6b3f21dcd728ad1fc71bad929db4c18132c69
Jeetesh Kumar Shaw <jeeteshshaw@gmail.com>
2021-08-07 18:04:51 +0530

    UseState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [count, setCount] = useState(0) //initial value
+  
+  // function Increment (name){
+  //   setCount(count+1)
+  // }
+  // function Decrement (){
+  //   setCount(count-1)
+  // }
+  const Increment = ()=>{
+    setCount(count+1)
+  }
+  const Decrement = ()=>{
+    setCount(count-1)
+  }
+  
   return (
     <div className="App">
-      <h3>Hello World</h3>
+      <button onClick={Increment}>+</button>
+       {count}
+      <button onClick={Decrement}>-</button>
     </div>
   );
 }
